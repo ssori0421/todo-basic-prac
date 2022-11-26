89524a24bdf02175a94a23bcab56188251fe3a77
ssori0421 <ssori0421@gmail.com>
2022-11-27 01:08:34 +0900

    header,input form layout

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return(
-      <div className='contaniner'>
-        <header className='header'>
-          <h3>My Todo List</h3>
-          <h3>React</h3>
-        </header>
-
-        <form className='inputForm'>
-          <div className='inputWrap'>
-            <lable className='inputLabel'>
-              <strong>제목</strong> 
-            </lable>
-            <input className='textInput' type='text'></input>
-            <label className='inputLabel'>
-              <strong>내용</strong>
-            </label>
-            <input className='textInput' type='text'></input> 
-          </div>
-          <button className='addButton'>추가하기</button>
-        </form> 
-      </div>
+  return (
+    <div className='container'>
+      <header className='header'>
+        <h3>My Todo List</h3>
+        <h3>React</h3>
+      </header>
+      <form className='inputForm'>
+        <div className='inputWrap'>
+          <lable className='inputLabel'>
+            <strong>제목</strong>
+          </lable>
+          <input className='textInput' type='text'></input>
+          <label className='inputLabel'>
+            <strong>내용</strong>
+          </label>
+          <input className='textInput' type='text'></input>
+        </div>
+        <button className='addButton'>추가하기</button>
+      </form>
+    </div>
   );
 }
 export default App;
